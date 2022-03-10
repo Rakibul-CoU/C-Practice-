260df747f49eba202db07e18d1c81e81bdc949d9
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2022-03-11 00:01:26 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arraycheck // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ",array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(array.Max());
+            Console.WriteLine(array.Min()); 
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
